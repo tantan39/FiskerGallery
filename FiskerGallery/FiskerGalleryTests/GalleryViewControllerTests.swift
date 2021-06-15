10d6a596407f524656539f1842f5b9e333298c8a
Tan Tran <tantan39>
2021-06-15 13:52:12 -0400

    Make `GalleryViewController` conform `UICollectionViewController`

--- FiskerGallery/FiskerGalleryTests/GalleryViewControllerTests.swift
+++ FiskerGallery/FiskerGalleryTests/GalleryViewControllerTests.swift
@@ -20,13 +20,17 @@ class GalleryViewModel {
     }
 }
 
-class GalleryViewController: UIViewController {
-    private(set) var viewModel: GalleryViewModel?
+class GalleryViewController: UICollectionViewController {
+    var viewModel: GalleryViewModel?
     
-    convenience init(viewModel: GalleryViewModel) {
-        self.init()
+    init(collectionViewLayout layout: UICollectionViewLayout = UICollectionViewLayout(), viewModel: GalleryViewModel) {
+        super.init(collectionViewLayout: layout)
         
         self.viewModel = viewModel
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
     }
     
     override func viewDidLoad() {
@@ -40,12 +44,10 @@ class GalleryViewControllerTests: XCTestCase {
         let sut = GalleryViewController(viewModel: GalleryViewModel(loader: loader))
         
         XCTAssertEqual(loader.loadGalleryCallCount, 0, "Expected no loading requests before view is loaded")
-    
+        
         sut.loadViewIfNeeded()
         XCTAssertEqual(loader.loadGalleryCallCount, 1, "Expected a loading request once view is loaded")
     }
-    
-    
 }
 
 extension GalleryViewControllerTests {
