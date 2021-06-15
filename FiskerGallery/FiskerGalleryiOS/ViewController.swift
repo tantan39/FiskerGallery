7bd242ad4993f1e9fc702c91d47f6b0da7fb2b69
Tan Tran <tantan39>
2021-06-15 15:16:58 -0400

    Add iOS platform target

---
+++ FiskerGallery/FiskerGalleryiOS/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FiskerGalleryiOS
+//
+//  Created by Tan Tan on 6/15/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
