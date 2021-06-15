0813a54cc1bb86f90b1240dab9764c462c66abf9
Tan Tran <tantan39>
2021-06-15 12:30:19 -0400

    Doesn't load gallery before view loaded and load gallery after view loaded

---
+++ FiskerGallery/FiskerGalleryTests/GalleryViewControllerTests.swift
@@ -0,0 +1,64 @@
+//
+//  GalleryViewControllerTests.swift
+//  FiskerGalleryTests
+//
+//  Created by Tan Tan on 6/15/21.
+//
+
+import XCTest
+import FiskerGallery
+
+class GalleryViewModel {
+    private(set) var loader: GalleryLoader?
+    
+    init(loader: GalleryLoader) {
+        self.loader = loader
+    }
+    
+    func fetchGallery() {
+        self.loader?.load(completion: { _ in })
+    }
+}
+
+class GalleryViewController: UIViewController {
+    private(set) var viewModel: GalleryViewModel?
+    
+    convenience init(viewModel: GalleryViewModel) {
+        self.init()
+        
+        self.viewModel = viewModel
+    }
+    
+    override func viewDidLoad() {
+        self.viewModel?.fetchGallery()
+    }
+}
+
+class GalleryViewControllerTests: XCTestCase {
+    func test_loadGalleryActions_requestGalleryFromLoader() {
+        let loader = LoaderSpy()
+        let sut = GalleryViewController(viewModel: GalleryViewModel(loader: loader))
+        
+        XCTAssertEqual(loader.loadGalleryCallCount, 0, "Expected no loading requests before view is loaded")
+    
+        sut.loadViewIfNeeded()
+        XCTAssertEqual(loader.loadGalleryCallCount, 1, "Expected a loading request once view is loaded")
+    }
+    
+    
+}
+
+extension GalleryViewControllerTests {
+    class LoaderSpy: GalleryLoader {
+    
+        private var requests = [(GalleryLoader.Result) -> Void]()
+        
+        var loadGalleryCallCount: Int {
+            return requests.count
+        }
+        
+        func load(completion: @escaping (GalleryLoader.Result) -> Void) {
+            requests.append(completion)
+        }
+    }
+}
